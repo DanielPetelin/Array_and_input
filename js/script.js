df7d486ad6b1fb222bbde54521f33646b2f68abd
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-06 17:35:47 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // проект про массив.
 // вписываем в инпут и жмем на кнопку.
 
-var a = [4, 5, 6];
+var a = [4, 5, 6, 123, 43, 12, 'Петя', 'Витя', 12, -12, 0, -0];
+// var t = []; - тут записать нельзя.
 
 function f1() {
+    
     var t = [];
     var b = +document.getElementById('nums1').value;
     t[0] = b;
@@ -15,6 +17,7 @@ function f1() {
     a = t;
 
     console.log(t);
+    document.getElementById('out').innerHTML = t;
 };
 
 document.getElementById('nums2').onclick = f1;
